2284372daf323f6ce6c1bf70a5afbdccadcec15b
LEll66 <969461192@qq.com>
2016-05-18 22:35:57 +0800

    could upload image,but crash sometimes

--- app/src/main/java/cn/ilell/ihome/FamilyFaceActivity.java
+++ app/src/main/java/cn/ilell/ihome/FamilyFaceActivity.java
@@ -1,283 +1,152 @@
 package cn.ilell.ihome;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.ClipData;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
-import java.io.File;
 
-import cn.ilell.ihome.utils.FileUtils;
+public class FamilyFaceActivity extends AppCompatActivity {
 
-/**
- * Created by lhc35 on 2016/5/6.
- */
-public class FamilyFaceActivity extends Activity {
-    public static final String TAG = "FamilyFaceActivity";
-    ValueCallback<Uri> mUploadMessage;
-    private WebView mWebView;
+    private final static String TAG = "FamilyFaceActivity";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_familyface);
-        initView();
-    }
+    private String TMP_URL = "http://115.159.127.79/ihome/intell_recognition.php";
+    private WebView webview;
 
-    private void initView() {
-        mWebView = (WebView) findViewById(R.id.web_view);
-        mWebView.setWebChromeClient(new MyWebChromeClient());
+    private ValueCallback<Uri> mUploadMessage;
+    private ValueCallback<Uri[]> mUploadCallbackAboveL;
 
-        mWebView.setWebViewClient(new MyWebViewClient(this));
-//		webView.loadUrl("file:///android_asset/upload_image.html");
-        WebSettings settings = mWebView.getSettings();
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+
+    private void assignViews() {
+        webview = (WebView) findViewById(R.id.webview);
+        WebSettings settings = webview.getSettings();
+        settings.setUseWideViewPort(true);
+        settings.setLoadWithOverviewMode(true);
         settings.setJavaScriptEnabled(true);
+        settings.setSupportZoom(true);
+        webview.setVerticalScrollBarEnabled(true);
+        webview.setHorizontalScrollBarEnabled(true);
         //打开网页时不调用系统浏览器， 而是在本WebView中显示
-        mWebView.setWebViewClient(new WebViewClient(){
+        webview.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
                 view.loadUrl(url);
                 return true;
             }
         });
-        mWebView.loadUrl("http://115.159.127.79/ihome/z-familyface.php");
-    }
+        webview.setWebChromeClient(new WebChromeClient(){
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                Log.d(TAG, "openFileChoose(ValueCallback<Uri> uploadMsg)");
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                FamilyFaceActivity.this.startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+            }
 
-
-    private class MyWebViewClient extends WebViewClient{
-        private Context mContext;
-        public MyWebViewClient(Context context){
-            super();
-            mContext = context;
-        }
-
-        @Override
-        public void onPageStarted(WebView view, String url, Bitmap favicon) {
-            Log.d(TAG,"URL地址:" + url);
-            super.onPageStarted(view, url, favicon);
-        }
-
-        @Override
-        public void onPageFinished(WebView view, String url) {
-            Log.i(TAG, "onPageFinished");
-            super.onPageFinished(view, url);
-        }
-    }
-
-    public static final int FILECHOOSER_RESULTCODE = 1;
-    private static final int REQ_CAMERA = FILECHOOSER_RESULTCODE+1;
-    private static final int REQ_CHOOSE = REQ_CAMERA+1;
-
-    private class MyWebChromeClient extends WebChromeClient {
-
-        // For Android 3.0+
-        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
-            if (mUploadMessage != null) return;
-            mUploadMessage = uploadMsg;
-            selectImage();
-//               Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-//               i.addCategory(Intent.CATEGORY_OPENABLE);
-//               i.setType("*/*");
-//                   startActivityForResult( Intent.createChooser( i, "File Chooser" ), FILECHOOSER_RESULTCODE );
-        }
-        // For Android < 3.0
-        public void openFileChooser(ValueCallback<Uri> uploadMsg) {
-            openFileChooser( uploadMsg, "" );
-        }
-        // For Android  > 4.1.1
-        public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
-            openFileChooser(uploadMsg, acceptType);
-        }
+            // For Android 3.0+
+            public void openFileChooser( ValueCallback uploadMsg, String acceptType ) {
+                Log.d(TAG, "openFileChoose( ValueCallback uploadMsg, String acceptType )");
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                FamilyFaceActivity.this.startActivityForResult(
+                        Intent.createChooser(i, "File Browser"),
+                        FILECHOOSER_RESULTCODE);
+            }
+            //For Android 4.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture){
+                Log.d(TAG, "openFileChoose(ValueCallback<Uri> uploadMsg, String acceptType, String capture)");
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                FamilyFaceActivity.this.startActivityForResult( Intent.createChooser( i, "File Browser" ), FamilyFaceActivity.FILECHOOSER_RESULTCODE );
+            }
+            // For Android 5.0+
+            public boolean onShowFileChooser (WebView webView, ValueCallback<Uri[]> filePathCallback, WebChromeClient.FileChooserParams fileChooserParams) {
+                Log.d(TAG, "onShowFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture)");
+                mUploadCallbackAboveL = filePathCallback;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                FamilyFaceActivity.this.startActivityForResult(
+                        Intent.createChooser(i, "File Browser"),
+                        FILECHOOSER_RESULTCODE);
+                return true;
+            }
+        });
+        webview.loadUrl(TMP_URL);
 
     }
 
-    /**
-     * 检查SD卡是否存在
-     *
-     * @return
-     */
-    public final boolean checkSDcard() {
-        boolean flag = Environment.getExternalStorageState().equals(
-                Environment.MEDIA_MOUNTED);
-        if (!flag) {
-            Toast.makeText(this, "请插入手机存储卡再使用本功能",Toast.LENGTH_SHORT).show();
-        }
-        return flag;
-    }
-    String compressPath = "";
-
-    protected final void selectImage() {
-        if (!checkSDcard())
-            return;
-        String[] selectPicTypeStr = { "camera","photo" };
-        new AlertDialog.Builder(this)
-                .setItems(selectPicTypeStr,
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog,
-                                                int which) {
-                                switch (which) {
-                                    // 相机拍摄
-                                    case 0:
-                                        openCarcme();
-                                        break;
-                                    // 手机相册
-                                    case 1:
-                                        chosePic();
-                                        break;
-                                    default:
-                                        break;
-                                }
-                                compressPath = Environment
-                                        .getExternalStorageDirectory()
-                                        .getPath()
-                                        + "/fuiou_wmp/temp";
-                                new File(compressPath).mkdirs();
-                                compressPath = compressPath + File.separator
-                                        + "compress.jpg";
-                            }
-                        }).show();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_familyface);
+        assignViews();
     }
 
-    String imagePaths;
-    Uri  cameraUri;
-    /**
-     * 打开照相机
-     */
-    private void openCarcme() {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-        imagePaths = Environment.getExternalStorageDirectory().getPath()
-                + "/fuiou_wmp/temp/"
-                + (System.currentTimeMillis() + ".jpg");
-        // 必须确保文件夹路径存在，否则拍照后无法完成回调
-        File vFile = new File(imagePaths);
-        if (!vFile.exists()) {
-            File vDirPath = vFile.getParentFile();
-            vDirPath.mkdirs();
-        } else {
-            if (vFile.exists()) {
-                vFile.delete();
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==FILECHOOSER_RESULTCODE)
+        {
+            if (null == mUploadMessage && null == mUploadCallbackAboveL) return;
+            Uri result = data == null || resultCode != RESULT_OK ? null : data.getData();
+            if (mUploadCallbackAboveL != null) {
+                onActivityResultAboveL(requestCode, resultCode, data);
+            }
+            else  if (mUploadMessage != null) {
+                mUploadMessage.onReceiveValue(result);
+                mUploadMessage = null;
             }
         }
-        cameraUri = Uri.fromFile(vFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, cameraUri);
-        startActivityForResult(intent, REQ_CAMERA);
     }
 
-    /**
-     * 拍照结束后
-     */
-    private void afterOpenCamera() {
-        File f = new File(imagePaths);
-        addImageGallery(f);
-        File newFile = FileUtils.compressFile(f.getPath(), compressPath);
-    }
-
-    /** 解决拍照后在相册中找不到的问题 */
-    private void addImageGallery(File file) {
-        ContentValues values = new ContentValues();
-        values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
-        values.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-        getContentResolver().insert(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-    }
-
-    /**
-     * 本地相册选择图片
-     */
-    private void chosePic() {
-        FileUtils.delFile(compressPath);
-        Intent innerIntent = new Intent(Intent.ACTION_GET_CONTENT); // "android.intent.action.GET_CONTENT"
-        String IMAGE_UNSPECIFIED = "image/*";
-        innerIntent.setType(IMAGE_UNSPECIFIED); // 查看类型
-        Intent wrapperIntent = Intent.createChooser(innerIntent, null);
-        startActivityForResult(wrapperIntent, REQ_CHOOSE);
-    }
-
-    /**
-     * 选择照片后结束
-     *
-     * @param data
-     */
-    private Uri afterChosePic(Intent data) {
-
-        // 获取图片的路径：
-        String[] proj = { MediaStore.Images.Media.DATA };
-        // 好像是android多媒体数据库的封装接口，具体的看Android文档
-        Cursor cursor = managedQuery(data.getData(), proj, null, null, null);
-        if(cursor == null ){
-            Toast.makeText(this, "上传的图片仅支持png或jpg格式",Toast.LENGTH_SHORT).show();
-            return null;
-        }
-        // 按我个人理解 这个是获得用户选择的图片的索引值
-        int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-        // 将光标移至开头 ，这个很重要，不小心很容易引起越界
-        cursor.moveToFirst();
-        // 最后根据索引值获取图片路径
-        String path = cursor.getString(column_index);
-        if(path != null && (path.endsWith(".png")||path.endsWith(".PNG")||path.endsWith(".jpg")||path.endsWith(".JPG"))){
-            File newFile = FileUtils.compressFile(path, compressPath);
-            return Uri.fromFile(newFile);
-        }else{
-            Toast.makeText(this, "上传的图片仅支持png或jpg格式",Toast.LENGTH_SHORT).show();
-        }
-        return null;
-    }
-
-
-    /**
-     * 返回文件选择
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent intent) {
-//		if (requestCode == FILECHOOSER_RESULTCODE) {
-//			if (null == mUploadMessage)
-//				return;
-//			Uri result = intent == null || resultCode != RESULT_OK ? null
-//					: intent.getData();
-//			mUploadMessage.onReceiveValue(result);
-//			mUploadMessage = null;
-//		}
-
-        if (null == mUploadMessage)
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    private void onActivityResultAboveL(int requestCode, int resultCode, Intent data) {
+        if (requestCode != FILECHOOSER_RESULTCODE
+                || mUploadCallbackAboveL == null) {
             return;
-        Uri uri = null;
-        if(requestCode == REQ_CAMERA ){
-            afterOpenCamera();
-            uri = cameraUri;
-        }else if(requestCode == REQ_CHOOSE){
-            uri = afterChosePic(intent);
         }
-        mUploadMessage.onReceiveValue(uri);
-        mUploadMessage = null;
-        super.onActivityResult(requestCode, resultCode, intent);
-    }
 
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
-            mWebView.goBack();
-            return true;
-        }else{
-            finish();
+        Uri[] results = null;
+        if (resultCode == Activity.RESULT_OK) {
+            if (data == null) {
+
+            } else {
+                String dataString = data.getDataString();
+                ClipData clipData = data.getClipData();
+
+                if (clipData != null) {
+                    results = new Uri[clipData.getItemCount()];
+                    for (int i = 0; i < clipData.getItemCount(); i++) {
+                        ClipData.Item item = clipData.getItemAt(i);
+                        results[i] = item.getUri();
+                    }
+                }
+
+                if (dataString != null)
+                    results = new Uri[]{Uri.parse(dataString)};
+            }
         }
-        return super.onKeyDown(keyCode, event);
+        mUploadCallbackAboveL.onReceiveValue(results);
+        mUploadCallbackAboveL = null;
+        return;
     }
 }

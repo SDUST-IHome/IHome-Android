4eeaca3abdd74f02438b9e07ccd4da04279b6830
unknown <>
2016-08-18 10:20:11 +0800

    regedit had changed

--- app/src/main/java/cn/ilell/ihome/RegistActivity.java
+++ app/src/main/java/cn/ilell/ihome/RegistActivity.java
@@ -22,12 +22,16 @@ import cn.ilell.ihome.utils.HttpXmlClient;
  */
 public class RegistActivity extends BaseSubPageActivity {
     private HttpClient http;
-    private Button btn_regedit;
-    private EditText text_user;
-    private EditText text_name;
-    private EditText text_phone;
-    private EditText text_pwd;
-    private EditText text_pwdenter;
+    private Button btn_regedit,
+                btn_verify;
+    private EditText text_homeid,
+                text_homepwd,
+                text_mail,
+                text_verify,
+                text_name,
+                text_phone,
+                text_pwd,
+                text_pwdenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,38 +47,90 @@ public class RegistActivity extends BaseSubPageActivity {
         http = new DefaultHttpClient();
 
         btn_regedit = (Button) findViewById(R.id.regist_btn_regist);
-        text_user = (EditText) findViewById(R.id.regist_text_user);
+        btn_verify = (Button) findViewById(R.id.regist_btn_verify);
+
+        text_homeid = (EditText) findViewById(R.id.regist_text_homeid);
+        text_homepwd = (EditText) findViewById(R.id.regist_text_homepwd);
+        text_mail = (EditText) findViewById(R.id.regist_text_mail);
+        text_verify = (EditText) findViewById(R.id.regist_text_verify);
         text_name = (EditText) findViewById(R.id.regist_text_name);
         text_phone = (EditText) findViewById(R.id.regist_text_phone);
         text_pwd = (EditText) findViewById(R.id.regist_text_pwd);
         text_pwdenter = (EditText) findViewById(R.id.regist_text_pwdenter);
 
         btn_regedit.setOnClickListener(this);
+        btn_verify.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.regist_btn_verify:
+                String str_mail = text_mail.getText().toString();
+                if(str_mail.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "请填写登陆邮箱", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                Map<String, String> params_temp = new HashMap<String, String>();
+                params_temp.put("email", str_mail);
+                String rul = HttpXmlClient.post("http://115.159.127.79/ihome/backdeal/Mailer.php", params_temp);
+                Toast.makeText(RegistActivity.this, rul, Toast.LENGTH_SHORT).show();
+                break;
             // FloatingActionButton的点击事件
             case R.id.regist_btn_regist:
-                String username,pwd,name,phonenum;
-                username = text_user.getText().toString();
-                pwd = text_pwd.getText().toString();
-                name = text_name.getText().toString();
-                phonenum = text_phone.getText().toString();
+                String homeid = text_homeid.getText().toString(),
+                        homepwd = text_homepwd.getText().toString(),
+                        mail = text_mail.getText().toString(),
+                        verify = text_verify.getText().toString(),
+                        name = text_name.getText().toString(),
+                        phone = text_phone.getText().toString(),
+                        pwd = text_pwd.getText().toString(),
+                        pwdenter = text_pwdenter.getText().toString();
+
+                if (homeid.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "HomeID不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (homepwd.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "家庭密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (mail.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "邮箱不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (verify.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "验证码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (name.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "姓名不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (pwd.isEmpty()) {
+                    Toast.makeText(RegistActivity.this, "密码不能为空", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (!pwd.equals(pwdenter)) {
+                    Toast.makeText(RegistActivity.this, "密码不一致", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
                 Map<String, String> params = new HashMap<String, String>();
-                params.put("Username", username);
-                params.put("Password", pwd);
-                params.put("Name", name);
-                params.put("Phonenum", phonenum);
+                params.put("homeid", homeid);
+                params.put("homeid_passwd", homepwd);
+                params.put("email", mail);
+                params.put("verify", verify);
+                params.put("username", name);
+                params.put("phonenum", phone);
+                params.put("passwd", pwd);
 
                 String xml = HttpXmlClient.post("http://115.159.127.79/ihome/backdeal/SaveNewUser.php", params);
                 Toast.makeText(RegistActivity.this, xml, Toast.LENGTH_SHORT).show();
                 if (xml.equals("注册成功")) {
                     SharedPreferences settings = getSharedPreferences("IHomeAccount", 0);
                     SharedPreferences.Editor editor = settings.edit();
-                    editor.putString("user",username);
+                    editor.putString("user",mail);
                     editor.putString("password",pwd);
                     editor.commit();
 
